fcf116df696053bed4a69e19eb956e64c64893e3
JonasHelming <jhelming@eclipsesource.com>
2013-05-17 13:40:18 +0200

    added ui plugin

--- com.eclipsesource.makeithappen.ui/src/com/eclipsesource/makeithappen/ui/MailOpener.java
+++ com.eclipsesource.makeithappen.ui/src/com/eclipsesource/makeithappen/ui/MailOpener.java
@@ -3,12 +3,15 @@ package com.eclipsesource.makeithappen.ui;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecp.core.ECPProject;
 import org.eclipse.emf.ecp.ui.util.ECPModelElementOpener;
+import org.eclipse.emf.edit.provider.AttributeValueWrapperItemProvider;
+import org.eclipse.swt.program.Program;
 
 public class MailOpener implements ECPModelElementOpener {
 
-	public void openModelElement(Object arg0, ECPProject arg1) {
-		// TODO Auto-generated method stub
-		
+	public void openModelElement(Object object, ECPProject arg1) {
+		AttributeValueWrapperItemProvider wrapper=(AttributeValueWrapperItemProvider)object;
+		String value = (String) wrapper.getValue();
+		Program.launch("mailto:"+value);
 	}
 
 	
