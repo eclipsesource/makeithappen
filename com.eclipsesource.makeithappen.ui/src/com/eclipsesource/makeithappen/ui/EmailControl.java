43695829a53ced900f608e45c24f440bb87cea69
eneufeld <neufeld.eugen@googlemail.com>
2013-10-23 19:35:51 +0200

    Updated Control to new ECP Version 1.1

--- com.eclipsesource.makeithappen.ui/src/com/eclipsesource/makeithappen/ui/EmailControl.java
+++ com.eclipsesource.makeithappen.ui/src/com/eclipsesource/makeithappen/ui/EmailControl.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 import java.net.URI;
 
 import org.eclipse.emf.ecp.edit.internal.swt.controls.StringControl;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
@@ -17,9 +18,11 @@ public class EmailControl extends StringControl {
 
 	@Override
 	protected void fillControlComposite(Composite composite) {
-		super.fillControlComposite(composite);
-		((GridLayout) composite.getLayout()).numColumns = 2;
-		Button button = new Button(composite, SWT.PUSH);
+		final Composite main = new Composite(composite, SWT.NONE);
+		GridLayoutFactory.fillDefaults().numColumns(2).applyTo(main);
+		GridDataFactory.fillDefaults().grab(true, false).align(SWT.FILL, SWT.BEGINNING).applyTo(main);
+		super.fillControlComposite(main);
+		Button button = new Button(main, SWT.PUSH);
 		button.setText("Send Mail");
 		button.addSelectionListener(new SelectionAdapter() {
 
@@ -27,9 +30,9 @@ public class EmailControl extends StringControl {
 			public void widgetSelected(SelectionEvent e) {
 				try {
 					Desktop.getDesktop().mail(
-							URI.create("mailto:" + getModelValue().getValue()));
+						URI.create("mailto:" + getModelValue().getValue()));
 				} catch (IOException e1) {
-					//TODO: Handle Exception
+					// TODO: Handle Exception
 				}
 			}
 
