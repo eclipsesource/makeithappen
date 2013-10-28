fa9f4de47da8c2a792fd33347bcebb197c2bf69d
Maximilian Koegel <maximilian@emfstore.org>
2013-10-28 14:03:38 +0100

    Fixed Email opener to use proper API

--- com.eclipsesource.makeithappen.ui/src/com/eclipsesource/makeithappen/ui/MailOpener.java
+++ com.eclipsesource.makeithappen.ui/src/com/eclipsesource/makeithappen/ui/MailOpener.java
@@ -1,16 +1,24 @@
 package com.eclipsesource.makeithappen.ui;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+
 import org.eclipse.emf.ecp.core.ECPProject;
 import org.eclipse.emf.ecp.ui.util.ECPModelElementOpener;
 import org.eclipse.emf.edit.provider.AttributeValueWrapperItemProvider;
-import org.eclipse.swt.program.Program;
 
 public class MailOpener implements ECPModelElementOpener {
 
 	public void openModelElement(Object object, ECPProject arg1) {
 		AttributeValueWrapperItemProvider wrapper=(AttributeValueWrapperItemProvider)object;
 		String value = (String) wrapper.getValue();
-		Program.launch("mailto:"+value);
+		try {
+			Desktop.getDesktop().mail(
+					URI.create("mailto:" + value));
+		} catch (IOException e) {
+			//ignore failure
+		}
 	}
 
 }
