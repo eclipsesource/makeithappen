9319b0172e93b8c58c61e15ec1c14849d7edc5c4
Maximilian Koegel <maximilian@emfstore.org>
2014-04-10 12:26:25 +0200

    adapted email control to new API

--- other/com.eclipsesource.makeithappen.ui.emailControl/src/com/eclipsesource/makeithappen/ui/emailcontrol/EmailControlRenderer.java
+++ other/com.eclipsesource.makeithappen.ui.emailControl/src/com/eclipsesource/makeithappen/ui/emailcontrol/EmailControlRenderer.java
@@ -1,10 +1,11 @@
-package com.eclipsesource.makeithappen.ui;
+package com.eclipsesource.makeithappen.ui.emailcontrol;
 
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 
-import org.eclipse.emf.ecp.edit.internal.swt.controls.StringControl;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
+import org.eclipse.emf.ecp.view.internal.core.swt.renderer.TextControlSWTRenderer;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
@@ -12,16 +13,18 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
 @SuppressWarnings("restriction")
-public class EmailControl extends StringControl {
+public class EmailControlRenderer extends TextControlSWTRenderer {
 
 	@Override
-	protected void fillControlComposite(Composite composite) {
-		final Composite main = new Composite(composite, SWT.NONE);
+	protected Control createSWTControl(Composite parent, final Setting setting) {
+		final Composite main = new Composite(parent, SWT.NONE);
 		GridLayoutFactory.fillDefaults().numColumns(2).applyTo(main);
-		GridDataFactory.fillDefaults().grab(true, false).align(SWT.FILL, SWT.BEGINNING).applyTo(main);
-		super.fillControlComposite(main);
+		GridDataFactory.fillDefaults().grab(true, false)
+				.align(SWT.FILL, SWT.BEGINNING).applyTo(main);
+		Control control = super.createSWTControl(main, setting);
 		Button button = new Button(main, SWT.PUSH);
 		button.setText("Send Mail");
 		button.addSelectionListener(new SelectionAdapter() {
@@ -29,12 +32,14 @@ public class EmailControl extends StringControl {
 			public void widgetSelected(SelectionEvent e) {
 				try {
 					Desktop.getDesktop().mail(
-						URI.create("mailto:" + getModelValue().getValue()));
+							URI.create("mailto:"
+									+ getModelValue(setting).getValue()));
 				} catch (IOException e1) {
-					//ignore failure to open mailto
+					// ignore failure to open mailto
 				}
 			}
 		});
+		return control;
 	}
 
 }
---
+++ other/com.eclipsesource.makeithappen.ui.emailControl/src/com/eclipsesource/makeithappen/ui/emailcontrol/EmailControlRendererTest.java
@@ -0,0 +1,30 @@
+package com.eclipsesource.makeithappen.ui.emailcontrol;
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecp.view.spi.context.ViewModelContext;
+import org.eclipse.emf.ecp.view.spi.model.VControl;
+import org.eclipse.emf.ecp.view.spi.model.VElement;
+import org.eclipse.emf.ecp.view.spi.swt.ECPRendererTester;
+
+import com.eclipsesource.makeithappen.model.task.TaskPackage;
+
+public class EmailControlRendererTest implements ECPRendererTester {
+
+	
+	public int isApplicableForFeature(EStructuralFeature feature, VElement vElement, ViewModelContext context) {
+		if (feature.equals(TaskPackage.eINSTANCE.getUser_Email())) {
+			return 10;
+		}
+		return NOT_APPLICABLE;
+	}
+	
+	@Override
+	public int isApplicable(VElement vElement, ViewModelContext context) {
+		if (!VControl.class.isInstance(vElement)) {
+			return NOT_APPLICABLE;
+		}
+		EStructuralFeature feature = VControl.class.cast(vElement).getDomainModelReference().getEStructuralFeatureIterator().next();
+		return isApplicableForFeature(feature, vElement, context);
+	}
+
+}
